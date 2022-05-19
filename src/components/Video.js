e94c042ba70ddc3e5e9197091a1ee546dc009063
eddiearnoldz <eddiearnoldz@me.com>
2022-05-19 13:55:22 -0700

    working on video page

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
+import gif from '../gifs/Dead in October - Trailer-low.gif'
+
 
 const Video = ({url}) => {
+  const [isLoaded, setIsLoaded] = useState(false)
   return (
-    <div className='relative pt-[56.25%]'>
-      <div className='absolute'>
+    <div className='p-3 h-full w-full'>
+      <div className=' hover:scale-105 duration-300 h-[300px]'>
         <ReactPlayer
         url={url}
         controls={true}
-        width='300px'
-        height='300px'
+        width='100%'
+        height='100%'
+        onReady={() => {setIsLoaded(true)}}
         />    
       </div>
    </div>
--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,11 +1,78 @@
-import React from 'react'
-import Video from '../components/Video'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import Video from '../components/Video';
+
+//import images
+import gif from '../gifs/Dead in October - Trailer-low.gif'
+import april from '../images/April.png';
+import percival from '../images/Percival.png';
+import champion from '../images/champion.png';
+import us from '../images/modern-love-us.png';
+import potions from '../images/potions.png';
+import follow from '../images/modern-love-follow.png';
+import dio from '../images/DIO.png';
+import mulimba from '../images/mulimba.png';
+import sepa from '../images/percival-sepa.png';
+
+
+
+
+const videos = [
+  {image: percival, url:"https://vimeo.com/710751949", text: 'Pericval'},
+  {image: april, url:  "https://vimeo.com/710753842", text: 'Modern Love'},
+  {image: champion, url: "https://vimeo.com/377951727", text: 'Modern Love'},
+  {image: us, url: "https://vimeo.com/673592625", text: 'Modern Love'},
+  {image: potions, url: "https://vimeo.com/678279129", text: 'Modern Love'},
+  {image: follow, url: "https://vimeo.com/710756878", text: 'Modern Love'},
+  {image: dio, url: "https://vimeo.com/673588688", text: 'Modern Love'},
+  {image: mulimba, url:"https://vimeo.com/710750869", text: 'Modern Love'},
+  {image: sepa, url: "https://vimeo.com/710756878", text: 'Modern Love'}
+]
 
 const Videos = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const handleVideoLoad = (e) => {
+    e.preventDefault();
+    setIsOpen(e.target.id)
+    window.scroll({top:0,behavior:'smooth'})
+  }
   return (
-    // <div className=" lg:h-[1000px] lg:w-[1000px] place-items-center">
-<div className='grid h-full w-[1000px] lg:grid-cols-3 grid-cols-1 place-items-center'> 
-      <Video url="https://vimeo.com/710756878"/>
+    <>
+    <div className="text-center">
+      {isOpen ? 
+        <div className="p-1">
+        <Video url={isOpen}/>
+        <button onClick={()=>setIsOpen(false)}>Back to videos</button>
+        </div> : <h1 className="mt-10">Work</h1>}
+    </div>
+     <div className="container h-full max-w-4xl w-full justify-center">
+      <div className="grid md:grid-cols-3 grid-cols-1 p-5 h-full">
+        {videos.map((image, index) => {
+          return (
+            <>
+            <div className="relative h-auto w-auto rounded-lg cursor-pointer object-fit m-auto p-1">
+              <img key={index} id={image.url} onClick={handleVideoLoad} className="rounded-md" src={image.image} alt='gif'/>
+              <div className="absolute top-1/2 left-1/2 text-center">
+              <h1 className="text-xl font-bold  text-heroSand">{image.text}</h1>
+              </div>
+            </div>
+            </>
+            
+          )
+        })}
+      </div> 
+    </div>
+    </>
+   
+      
+  )
+}
+
+export default Videos
+
+{/* <Video url="https://vimeo.com/710756878"/>
       <Video url="https://vimeo.com/710753842"/>
       <Video url="https://vimeo.com/377951727"/>
       <Video url="https://vimeo.com/673592625"/>
@@ -13,11 +80,4 @@ const Videos = () => {
       <Video url="https://vimeo.com/710756878"/>
       <Video url="https://vimeo.com/673588688"/>
       <Video url="https://vimeo.com/710750869"/>
-      <Video url="https://vimeo.com/710751949"/>
-    {/* </div>  */}
-    </div>
-      
-  )
-}
-
-export default Videos
+      <Video url="https://vimeo.com/710751949"/> */}
