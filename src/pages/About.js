c9bd74ead5f586a8863031a65c8c87c62736d8a5
eddiearnoldz <eddiearnoldz@me.com>
2022-05-23 11:32:03 -0700

    about page mobile layout set up

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import liam from '../images/liam-profile.gif'
-import mancAudience from '../images/manchester-audience-award.png'
+import mancAudience from '../images/manchester-best-score.png'
 import manScore from '../images/manchester-audience-award.png'
 import nottingham from '../images/nottingham-selection.png'
 import ramsgate from '../images/ramsgate-selection.png'
@@ -9,32 +9,27 @@ import wonderland from '../images/wonderland-quote.png'
 const About = () => {
   return (
     <div className='grid place-items-center h-full w-full'>
-      <div className="content-center m-3 mt-10">
+      <div className="content-center m-3 mt-10 w-auto">
         <img className="rounded-md w-96 m-auto" src={liam} alt='liam'/>
-        <p className="text-center mt-10">      
-          Award winning filmmaker from Bradford, West Yorkshire; based in London.
+        <p className="text-center mt-10 md:text-xl text-lg">      
+          Award winning filmmaker from Bradford, West Yorkshire; based in London
         </p>
-        <p className="text-center mt-2 font-semibold text-xl">
+        <p className="text-center mt-2 font-semibold md:text-xl text-lg">
           Camera-op; Editor; Colourist; AD; Writer; Director
         </p>
-        <p className="text-center mt-2">
-          Get in touch and I'll bang the kettle on. 
+        <p className="text-center mt-2 md:text-xl text-lg">
+          Get in touch and I'll bang the kettle on 
         </p>
       </div>
-      <div className="text-center mt-5 mb-5">
-        <p className='font-semibold'>
+      <div className="text-center m-5">
+        <p className='font-semibold md:text-lg text-md'>
         Writer and director of award winning feature, Dead in October 
         </p>
-        <div className='flex'>
-          <div className='md:flex block'>
-            <img className="md:h-20  h-32"src={mancAudience} alt='laurel'/>
-            <img className="md:h-20 h-36" src={nottingham} alt='laurel'/>
-            
-          </div>
-          <div className='md:flex block'>
-            <img className="md:h-20 h-32 " src={manScore} alt='laurel'/>
-            <img className="md:h-20 h-32 " src={ramsgate} alt='laurel'/>
-          </div>
+        <div className='md:flex justify-center align-center w-full m-1'>
+          <img className="h-36 inline-block" src={ramsgate} alt='laurel'/>
+          <img className="h-36 inline-block"src={mancAudience} alt='laurel'/>
+          <img className="h-36 inline-block " src={nottingham} alt='laurel'/>
+          <img className="h-36 inline-block" src={manScore} alt='laurel'/> 
         </div>
       </div>
       <img className="max-w-xs mb-10" src={wonderland} alt='wonderland quote'/>
