6c52f9f1c4d397aa907fccbadec44ede68eef93b
eddiearnoldz <eddiearnoldz@me.com>
2022-05-23 09:42:40 -0700

    about page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import liam from '../images/liam-headshot.jpg'
+import liam from '../images/liam-profile.gif'
+import mancAudience from '../images/manchester-audience-award.png'
+import manScore from '../images/manchester-audience-award.png'
+import nottingham from '../images/nottingham-selection.png'
+import ramsgate from '../images/ramsgate-selection.png'
+import wonderland from '../images/wonderland-quote.png'
 
 const About = () => {
   return (
-    <div className='grid place-items-center h-full'>
-      <div className="content-center sm:max-w-md max-w-sm mt-10 m-5">
-        <img className="rounded-lg h-60 m-auto" src={liam} alt='liam'/>
-        <p className="text-center mt-10">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit elementum tempor. Praesent feugiat magna vel quam hendrerit sodales. Sed sodales pulvinar ligula. Proin posuere luctus neque quis ullamcorper. Nullam pharetra aliquet pharetra. 
-        Etiam massa nunc, egestas quis urna sed, ultrices eleifend nisi. Quisque tincidunt hendrerit nunc, ac interdum mauris pretium vitae.
+    <div className='grid place-items-center h-full w-full'>
+      <div className="content-center m-2 mt-10">
+        <img className="rounded-md h-80 m-auto" src={liam} alt='liam'/>
+        <p className="text-center mt-10">      
+          Award winning filmmaker from Bradford, West Yorkshire; based in London.
+        </p>
+        <p className="text-center mt-2 font-semibold text-xl">
+          Camera-op; Editor; Colourist; AD; Writer; Director
+        </p>
+        <p className="text-center mt-2">
+          Get in touch and I'll bang the kettle on. 
         </p>
       </div>
+      <div className="text-center mt-5 mb-5">
+        <p className='font-semibold'>
+        Writer and director of award winning feature, Dead in October 
+        </p>
+        <div className='flex'>
+          <div className='md:flex block'>
+            <img className="md:h-20  h-32"src={mancAudience} alt='laurel'/>
+            <img className="md:h-20 h-36" src={nottingham} alt='laurel'/>
+            
+          </div>
+          <div className='md:flex block'>
+            <img className="md:h-20 h-32 " src={manScore} alt='laurel'/>
+            <img className="md:h-20 h-32 " src={ramsgate} alt='laurel'/>
+          </div>
+        </div>
+      </div>
+      <img className="max-w-xs mb-10" src={wonderland} alt='wonderland quote'/>
     </div>
     
   )
